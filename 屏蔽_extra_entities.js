7b68a4d967cefb5e2ed2c056338b913555f195eb
HuyAng49 <58811512+HuyAng49@users.noreply.github.com>
2023-11-19 21:02:50 +0800

    Update 屏蔽_extra_entities.js

--- 屏蔽_extra_entities.js
+++ 屏蔽_extra_entities.js
@@ -1,6 +1,18 @@
 let body = $response.body;
 
-// 替换或删除 extra_entities 字段下的内容
-body = body.replace(/"extra_entities":\{[^}]*\},/, '');
+try {
+    let jsonBody = JSON.parse(body);
+
+    // 检查是否存在 "data" 和 "extra_entities" 字段
+    if (jsonBody && jsonBody.data && jsonBody.data.extra_entities) {
+        // 将 extra_entities 字段设置为空数组
+        jsonBody.data.extra_entities = [];
+        
+        // 更新响应体
+        body = JSON.stringify(jsonBody);
+    }
+} catch (error) {
+    console.log("Error parsing JSON:", error);
+}
 
 $done({ body });
