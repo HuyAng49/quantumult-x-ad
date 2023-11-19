231b60155424fc99c5fe9bfe6801ded4fa21cb2a
HuyAng49 <58811512+HuyAng49@users.noreply.github.com>
2023-11-19 21:16:10 +0800

    Update 屏蔽_extra_entities.js

--- 屏蔽_extra_entities.js
+++ 屏蔽_extra_entities.js
@@ -1,7 +1,11 @@
+console.log($response,'1111')
 if ($response) {
     if ($response.body) {
+        console.log($response.body,'2222')
         try {
             let jsonBody = JSON.parse($response.body);
+            console.log(jsonBody,'3333')
+            console.log(jsonBody.data,'4444')
 
             // 检查是否存在 "data" 和 "extra_entities" 字段
             if (jsonBody && jsonBody.data && jsonBody.data.extra_entities) {
@@ -10,6 +14,7 @@ if ($response) {
                 
                 // 更新响应体
                 $done({ body: JSON.stringify(jsonBody) });
+                console.log($response.body,'4444')
             } else {
                 console.log("Invalid JSON structure or missing fields in the response body");
                 $done({});
