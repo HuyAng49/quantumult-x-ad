b41764c4867ca19074ee229bc7535d1a3160d916
HuyAng49 <58811512+HuyAng49@users.noreply.github.com>
2023-11-19 21:11:31 +0800

    Update 屏蔽_extra_entities.js

--- 屏蔽_extra_entities.js
+++ 屏蔽_extra_entities.js
@@ -1,23 +1,28 @@
-if ($response && $response.body) {
-    try {
-        let jsonBody = JSON.parse($response.body);
+if ($response) {
+    if ($response.body) {
+        try {
+            let jsonBody = JSON.parse($response.body);
 
-        // 检查是否存在 "data" 和 "extra_entities" 字段
-        if (jsonBody && jsonBody.data && jsonBody.data.extra_entities) {
-            // 将 extra_entities 字段设置为空数组
-            jsonBody.data.extra_entities = [];
-            
-            // 更新响应体
-            $done({ body: JSON.stringify(jsonBody) });
-        } else {
-            console.log("Invalid JSON structure or missing fields");
+            // 检查是否存在 "data" 和 "extra_entities" 字段
+            if (jsonBody && jsonBody.data && jsonBody.data.extra_entities) {
+                // 将 extra_entities 字段设置为空数组
+                jsonBody.data.extra_entities = [];
+                
+                // 更新响应体
+                $done({ body: JSON.stringify(jsonBody) });
+            } else {
+                console.log("Invalid JSON structure or missing fields in the response body");
+                $done({});
+            }
+        } catch (error) {
+            console.log("Error parsing JSON:", error);
             $done({});
         }
-    } catch (error) {
-        console.log("Error parsing JSON:", error);
+    } else {
+        console.log("Response body is empty");
         $done({});
     }
 } else {
-    console.log("Invalid $response object or missing body");
+    console.log("Invalid $response object");
     $done({});
 }
