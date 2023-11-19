3f62af3742301de83a9bd633766e55761dec1a1b
HuyAng49 <58811512+HuyAng49@users.noreply.github.com>
2023-11-19 21:05:11 +0800

    Update 屏蔽_extra_entities.js

--- 屏蔽_extra_entities.js
+++ 屏蔽_extra_entities.js
@@ -1,18 +1,23 @@
-let body = $response.body;
+if ($response && $response.body) {
+    let body = $response.body;
 
-try {
-    let jsonBody = JSON.parse(body);
+    try {
+        let jsonBody = JSON.parse(body);
 
-    // 检查是否存在 "data" 和 "extra_entities" 字段
-    if (jsonBody && jsonBody.data && jsonBody.data.extra_entities) {
-        // 将 extra_entities 字段设置为空数组
-        jsonBody.data.extra_entities = [];
-        
-        // 更新响应体
-        body = JSON.stringify(jsonBody);
+        // 检查是否存在 "data" 和 "extra_entities" 字段
+        if (jsonBody && jsonBody.data && jsonBody.data.extra_entities) {
+            // 将 extra_entities 字段设置为空数组
+            jsonBody.data.extra_entities = [];
+            
+            // 更新响应体
+            body = JSON.stringify(jsonBody);
+        }
+    } catch (error) {
+        console.log("Error parsing JSON:", error);
     }
-} catch (error) {
-    console.log("Error parsing JSON:", error);
-}
 
-$done({ body });
+    $done({ body });
+} else {
+    console.log("Invalid $response object or missing body");
+    $done({});
+}
